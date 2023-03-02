a0a7711c5ed39e892ccdc17047e02bcfeef720e8
Myrin <myrin.w@outlook.com>
2023-03-02 12:05:15 +0100

    commit

--- src/pages/pledge/pledge.js
+++ src/pages/pledge/pledge.js
@@ -37,7 +37,221 @@ function Faq() {
 				<div className="container">
 					<h3 className="pledge__title">
 						Preparing Aspiring Futures Traders for Live Futures Markets
+						<div className="pledge__number">01</div>
 					</h3>
+					<div className="pledge__line"></div>
+					<p className="pledge__text">
+						<strong>TEGA</strong> Members are primarily focused on helping the
+						aspiring futures trader community learn and develop trading and risk
+						management skills in a low risk, real-time simulated environment.
+						<strong>TEGA</strong> Members strongly believe that the experience
+						of simulated trading, as experiential learning, is a valuable
+						educational piece that will help increase the likelihood of trader
+						success. Members provide a real-time simulated trading environment
+						to help aspiring futures traders safely learn how to trade and to
+						develop the skills and the discipline necessary to be more prepared
+						for live trading, whether it be their personal or proprietary
+						capital. <strong>TEGA</strong> Members believe the service provided
+						is good for the trading community and the financial industry as a
+						whole.
+					</p>
+				</div>
+			</section>
+			<section className="pledge">
+				<div className="container">
+					<h3 className="pledge__title">
+						Providing Equal Opportunity for Aspiring Futures Traders
+						<div className="pledge__number">02</div>
+					</h3>
+					<div className="pledge__line"></div>
+					<p className="pledge__text">
+						<strong>TEGA</strong> Members firmly believe that success in trading
+						is not exclusively contingent on a formal or professional
+						background, and that the current technological environment has
+						opened up opportunities for trading in a democratic way. While in
+						the past, trading was often the exclusive domain of a closed, small
+						group of professionals, non-professional traders are testing their
+						mettle in the market in growing numbers. TEGA Members endeavor to
+						support the rights of, and provide opportunities for aspiring
+						futures traders to join the professional trading industry, by
+						testing themselves through an objective examination and joining a
+						proprietary trading firm as professional traders.
+						<br /> <br />
+						Except where legally prohibited, <strong>TEGA</strong> Members
+						commit to provide aspiring futures traders, regardless of physical
+						location, race, religious beliefs, age, gender, income level or
+						other differentiation, with access to a process that objectively
+						measures performance and ultimately leads to the possibility of
+						trading proprietary capital for those who meet the applicable
+						standards.
+					</p>
+				</div>
+			</section>
+			<section className="pledge">
+				<div className="container">
+					<h3 className="pledge__title">
+						Provide a Fair, Objective and Low Risk Environment for Aspiring
+						Futures Traders
+						<div className="pledge__number">03</div>
+					</h3>
+					<div className="pledge__line"></div>
+					<p className="pledge__text">
+						TEGA Members undertake to provide an objective and low risk
+						environment for aspiring futures traders to receive exposure to
+						markets. Accordingly, TEGA Members commit to the following
+						principles:
+						<br />
+						<br />
+						<ul>
+							<li class="pledge__list">
+								Provide an objective service for aspiring futures trader to have
+								their trading evaluated
+							</li>
+							<li class="pledge__list">
+								Provide aspiring futures traders with the option of at least one
+								live third party data feed connectivity service for their
+								evaluation
+							</li>
+							<li class="pledge__list">
+								Provide aspiring futures traders with the option of at least one
+								third party trading platform for their evaluation
+							</li>
+							<li class="pledge__list">
+								Ensure the impartiality of and prevention of manipulation of any
+								trades made over the course of the examination, and commit to
+								non interference or manipulation with any part of the data feed
+							</li>
+							<li class="pledge__list">
+								Commit to respond in a reasonably prompt and effective manner to
+								any customer support, billing, and technical support requests,
+								including directing traders with platform or data inquiries to
+								their respective providers
+							</li>
+							<li class="pledge__list">
+								Ensure that successful candidates who successfully complete
+								their evaluations are reasonably promptly onboarded to
+								proprietary trading accounts, without inappropriate delays
+							</li>
+							<li class="pledge__list">
+								Recognize that they are not registered Commodity Trading
+								Advisors (CTA’s) and refrain from providing any specific or
+								tailored trading advice to aspiring futures traders.
+							</li>
+						</ul>
+					</p>
+				</div>
+			</section>
+			<section className="pledge">
+				<div className="container">
+					<h3 className="pledge__title">
+						Practice Transparent Business Policies
+						<div className="pledge__number">04</div>
+					</h3>
+					<div className="pledge__line"></div>
+					<p className="pledge__text">
+						TEGA Members undertake to provide an objective and low risk
+						environment for aspiring futures traders to receive exposure to
+						markets. Accordingly, TEGA Members commit to the following
+						principles:
+						<br />
+						<br />
+						<ul>
+							<li class="pledge__list">
+								Provide an objective service for aspiring futures trader to have
+								their trading evaluated
+							</li>
+							<li class="pledge__list">
+								Provide aspiring futures traders with the option of at least one
+								live third party data feed connectivity service for their
+								evaluation
+							</li>
+							<li class="pledge__list">
+								Provide aspiring futures traders with the option of at least one
+								third party trading platform for their evaluation
+							</li>
+							<li class="pledge__list">
+								Ensure the impartiality of and prevention of manipulation of any
+								trades made over the course of the examination, and commit to
+								non interference or manipulation with any part of the data feed
+							</li>
+							<li class="pledge__list">
+								Commit to respond in a reasonably prompt and effective manner to
+								any customer support, billing, and technical support requests,
+								including directing traders with platform or data inquiries to
+								their respective providers
+							</li>
+							<li class="pledge__list">
+								Ensure that successful candidates who successfully complete
+								their evaluations are reasonably promptly onboarded to
+								proprietary trading accounts, without inappropriate delays
+							</li>
+							<li class="pledge__list">
+								Recognize that they are not registered Commodity Trading
+								Advisors (CTA’s) and refrain from providing any specific or
+								tailored trading advice to aspiring futures traders.
+							</li>
+						</ul>
+					</p>
+				</div>
+			</section>
+			<section className="pledge">
+				<div className="container">
+					<h3 className="pledge__title">
+						Providing Equal Opportunity for Aspiring Futures Traders
+						<div className="pledge__number">05</div>
+					</h3>
+					<div className="pledge__line"></div>
+					<p className="pledge__text">
+						<strong>TEGA</strong> Members firmly believe that success in trading
+						is not exclusively contingent on a formal or professional
+						background, and that the current technological environment has
+						opened up opportunities for trading in a democratic way. While in
+						the past, trading was often the exclusive domain of a closed, small
+						group of professionals, non-professional traders are testing their
+						mettle in the market in growing numbers. TEGA Members endeavor to
+						support the rights of, and provide opportunities for aspiring
+						futures traders to join the professional trading industry, by
+						testing themselves through an objective examination and joining a
+						proprietary trading firm as professional traders.
+						<br /> <br />
+						Except where legally prohibited, <strong>TEGA</strong> Members
+						commit to provide aspiring futures traders, regardless of physical
+						location, race, religious beliefs, age, gender, income level or
+						other differentiation, with access to a process that objectively
+						measures performance and ultimately leads to the possibility of
+						trading proprietary capital for those who meet the applicable
+						standards.
+					</p>
+				</div>
+			</section>
+			<section className="pledge pledge--last">
+				<div className="container">
+					<h3 className="pledge__title">
+						Providing Equal Opportunity for Aspiring Futures Traders
+						<div className="pledge__number">06</div>
+					</h3>
+					<div className="pledge__line"></div>
+					<p className="pledge__text">
+						<strong>TEGA</strong> Members firmly believe that success in trading
+						is not exclusively contingent on a formal or professional
+						background, and that the current technological environment has
+						opened up opportunities for trading in a democratic way. While in
+						the past, trading was often the exclusive domain of a closed, small
+						group of professionals, non-professional traders are testing their
+						mettle in the market in growing numbers. TEGA Members endeavor to
+						support the rights of, and provide opportunities for aspiring
+						futures traders to join the professional trading industry, by
+						testing themselves through an objective examination and joining a
+						proprietary trading firm as professional traders.
+						<br /> <br />
+						Except where legally prohibited, <strong>TEGA</strong> Members
+						commit to provide aspiring futures traders, regardless of physical
+						location, race, religious beliefs, age, gender, income level or
+						other differentiation, with access to a process that objectively
+						measures performance and ultimately leads to the possibility of
+						trading proprietary capital for those who meet the applicable
+						standards.
+					</p>
 				</div>
 			</section>
 			<section className="members">
