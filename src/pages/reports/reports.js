c67435bf9a1a919ca399363c3d43450dfcb692fd
Myrin <myrin.w@outlook.com>
2023-03-05 20:43:17 +0100

    reports

--- src/pages/reports/reports.js
+++ src/pages/reports/reports.js
@@ -56,7 +56,7 @@ function Reports() {
 						/>
 					</svg>
 
-					<h3 class="report__title">Green</h3>
+					<h3 class="report__title">Yellow</h3>
 					<div className="report__line"></div>
 					<p className="report__text">
 						Non-compliant, deviation from pledge determined. Action required to
@@ -109,11 +109,56 @@ function Reports() {
 							fill-rule="evenodd"
 							clip-rule="evenodd"
 							d="M29.9978 20.0857L29.9978 20.0858L29.993 20.094L9.56304 55.621C1.90284 68.945 -1.92496 75.609 0.961441 80.601C3.84814 85.5932 11.5354 85.5932 26.9024 85.5932H67.7614C83.1284 85.5932 90.8124 85.5932 93.7024 80.601C96.5891 75.6088 92.761 68.945 85.0969 55.621L64.6709 20.094L64.6689 20.0905C56.9676 6.69863 53.1159 0.000817629 47.332 0C41.5486 0.000817461 37.6975 6.69725 29.9978 20.0857ZM47.5128 17C50.6496 17 53.1718 19.5816 53.0988 22.7175L52.3674 54.1592C52.3059 56.8024 50.1456 58.913 47.5017 58.913C44.8564 58.913 42.6955 56.8003 42.6359 54.1558L41.9266 22.7136C41.8559 19.5791 44.3775 17 47.5128 17Z"
+							fill="#BF0707"
+						/>
+					</svg>
+
+					<h3 class="report__title">Red</h3>
+					<div className="report__line"></div>
+					<p className="report__text">
+						Non-compliant, serious deviation from pledge determined. Immediate
+						action required to address non-compliance. Red issues include:
+					</p>
+					<br />
+					<br />
+					<ul>
+						<li class="list-item">
+							Repeated customer complaints received by the association for
+							withdrawal, broker, or other issues
+						</li>
+						<li class="list-item">
+							Lack of transparency determined regarding simulated and live
+							funded accounts
+						</li>
+						<li class="list-item">
+							Reports of changing rules for live customer accounts
+						</li>
+						<li class="list-item">
+							Reports of email spamming and/or cold calling
+						</li>
+						<li class="list-item">Buying and or selling of customer data</li>
+					</ul>
+				</div>
+			</section>
+			<section className="report report--v2">
+				<div className="container">
+					<svg
+						width="95"
+						height="86"
+						viewBox="0 0 95 86"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+						className="report__svg"
+					>
+						<path
+							fill-rule="evenodd"
+							clip-rule="evenodd"
+							d="M29.9978 20.0857L29.9978 20.0858L29.993 20.094L9.56304 55.621C1.90284 68.945 -1.92496 75.609 0.961441 80.601C3.84814 85.5932 11.5354 85.5932 26.9024 85.5932H67.7614C83.1284 85.5932 90.8124 85.5932 93.7024 80.601C96.5891 75.6088 92.761 68.945 85.0969 55.621L64.6709 20.094L64.6689 20.0905C56.9676 6.69863 53.1159 0.000817629 47.332 0C41.5486 0.000817461 37.6975 6.69725 29.9978 20.0857ZM47.5128 17C50.6496 17 53.1718 19.5816 53.0988 22.7175L52.3674 54.1592C52.3059 56.8024 50.1456 58.913 47.5017 58.913C44.8564 58.913 42.6955 56.8003 42.6359 54.1558L41.9266 22.7136C41.8559 19.5791 44.3775 17 47.5128 17Z"
 							fill="black"
 						/>
 					</svg>
 
-					<h3 class="report__title">Green</h3>
+					<h3 class="report__title">Black</h3>
 					<div className="report__line"></div>
 					<p className="report__text">
 						Non-compliant, unacceptable deviation from pledge determined.
@@ -123,19 +168,33 @@ function Reports() {
 					<br />
 					<ul>
 						<li class="list-item">
-							Provide an objective service for aspiring futures trader to have
-							their trading evaluated
+							Manipulation of data, trades or trading records
 						</li>
 						<li class="list-item">
-							Provide aspiring futures traders with the option of at least one
-							live third party data feed connectivity service for their
-							evaluation
+							Claiming to provide a live account while actually providing a sim
+							account and vice-versa
 						</li>
 						<li class="list-item">
-							Provide aspiring futures traders with the option of at least one
-							third party trading platform for their evaluation
+							Providing specific trading advice to candidates and/or traders
+						</li>
+						<li class="list-item">
+							Permitting traders in the prop firm to in any way finance their
+							own accounts (resetting live accounts, depositing funds into the
+							live accounts, etc.)
 						</li>
 					</ul>
+				</div>
+			</section>
+			<section className="text">
+				<div className="container">
+					<h3 className="text__title">Penalties</h3>
+					<div className="text__line"></div>
+					<p className="text__text">
+						Penalties for non compliance, can include fines, posting visible
+						notices of the infraction on the website, or expulsion from the
+						association. Compliance reports are posted on the association
+						website.
+					</p>
 				</div>
 			</section>
 		</div>
