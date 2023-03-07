d7a3f4c6742e205fc51ecf9f1bed2eb5eda53c11
Myrin <myrin.w@outlook.com>
2023-03-07 17:36:43 +0100

    commit

--- src/components/hero-main/hero-main.js
+++ src/components/hero-main/hero-main.js
@@ -1,17 +1,25 @@
 function HeroMain(props) {
 	return (
 		<section className="hero hero--main">
-			<h1 className="hero__title">Trader Evaluation Governing Association</h1>
-			<p className="hero__text">
-				<strong>TEGA</strong> focuses primarily on assisting the trader
-				evaluation community by providing a vehicle for members to cooperate to
-				achieve their professional goals.
-			</p>
-			<img
-				className="hero__image"
-				src={require("../../img/stonks.png")}
-				alt=""
-			/>
+			<div className="container">
+				<h1 className="hero__title">Trader Evaluation Governing Association</h1>
+				<p className="hero__text">
+					<strong>TEGA</strong> focuses primarily on assisting the trader
+					evaluation community by providing a vehicle for members to cooperate
+					to achieve their professional goals.
+				</p>
+				{/* <img
+					className="hero__image"
+					src={require("../../img/homegraphs.png")}
+					alt=""
+				/> */}
+
+				<p className="hero__textblock text-block text-block--lg">
+					Launched in 2022 in response to the rapid growth of the trader
+					evaluation industry, TEGA endeavors to represent community interests
+					and provide an accessible forum for members to deliberate.
+				</p>
+			</div>
 		</section>
 	);
 }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,14 +1,9 @@
 import HeroMain from "../../components/hero-main/hero-main";
-import IntroText from "../../components/intro-text/intro-text";
 function Home() {
 	return (
 		<div>
 			<HeroMain />
-			<IntroText largespacing="true">
-				Launched in 2022 in response to the rapid growth of the trader
-				evaluation industry, TEGA endeavors to represent community interests and
-				provide an accessible forum for members to deliberate.
-			</IntroText>
+
 			<section className="emblem">
 				<div className="container">
 					<h2 className="emblem__title">Join Us and get certified</h2>
